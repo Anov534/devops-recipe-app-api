3cfe0b192ad100cfaa0f9a9cf943f62c492fef94
armanbiz <armanmkhitaryanbiz@gmail.com>
2025-01-14 22:09:17 +0100

    added TF

---
+++ Infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops1-recipe-app-api-state"
+    key            = "tf-state-setup"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
